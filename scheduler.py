63efe47649e6ef10d9645d1236998392655fe530
malavika-dileep <116961377+malavika-dileep@users.noreply.github.com>
2023-10-18 22:07:43 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -4,31 +4,29 @@ import time
 import schedule
 import pandas as pd
 from fetching import gettingdata
-from trips import tripone
 
 # Global variables
 release = 2023
 firsturl = "https://www.imdb.com/search/title/?title_type=feature,tv_series&year=2023-01-01,2023-12-31&explore=countries&countries=IN"
-recordsinimdb = 2200
 
 # Function to run the scraping job
 def run_scraping_job():
     print("Starting scraping job...")
     try:
-        # Call fetching.gettingdata to get TTN numbers
-        movies_from_scrape = gettingdata(release, firsturl)
-        
-        # Read the existing CSV file
-        existing_ttns = set(pd.read_csv('z1.csv')['TTN'].tolist())
-        
-        # Filter TTN numbers that are not in the CSV
-        new_ttns = [movie[0] for movie in movies_from_scrape if movie[0] not in existing_ttns]
-        
-        # If there are new TTN numbers, get the full movie details
+        # Call fetching.gettingdata to get IMDb IDs (TTN)
+        imdb_ids_from_scrape = [movie[0] for movie in gettingdata(release, firsturl)]
+
+        # Read the existing CSV file and get the IMDb IDs
+        existing_ttns = set(pd.read_csv('z1.csv')['TTN'])
+
+        # Filter IMDb IDs that are not in the CSV
+        new_ttns = [ttn for ttn in imdb_ids_from_scrape if ttn not in existing_ttns]
+
+        # If there are new IMDb IDs, get the full movie details
         new_movies = []
         if new_ttns:
             for ttn in new_ttns:
-                movie_details = gettingdata(release, ttn)  # Assuming gettingdata can accept TTN and fetch details
+                movie_details = gettingdata(release, f"https://www.imdb.com/title/{ttn}/")  # Assuming gettingdata can accept TTN and fetch details
                 if movie_details:
                     new_movies.append(movie_details)
             print(f"Scraped {len(new_movies)} new movies.")
